a0e801de9130c5e75c1194658d44e1ea7a305c9e
lordbaah <lordbaah2@gmail.com>
2025-08-17 17:33:15 +0000

    feat: added navigation buttons in new note and view note components

--- client/src/pages/NewNote.tsx
+++ client/src/pages/NewNote.tsx
@@ -1,9 +1,17 @@
 import NewNoteForm from '../components/NewNoteForm';
+import { useNavigate } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 const NewNote = () => {
+  const navigate = useNavigate();
   return (
     <section>
       <div className="max-w-5xl mx-auto px-6 md:px-4 py-8">
+        <div className="mb-4">
+          <button onClick={() => navigate(-1)} className="btn-primary">
+            <ChevronLeft className="icon-size" />
+          </button>
+        </div>
         <h1 className="text-xl mb-8">Create New Note</h1>
         <NewNoteForm />
       </div>
--- client/src/pages/ViewNotePage.tsx
+++ client/src/pages/ViewNotePage.tsx
@@ -1,17 +1,29 @@
 import { useParams } from 'react-router-dom';
 import ViewAndEditNote from '../components/ViewAndEditNote';
+import { useNavigate } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 const ViewNotePage = () => {
   const { noteId } = useParams();
+  const navigate = useNavigate();
   // console.log(useParams());
 
   if (!noteId) {
-    return <div>Error: Note ID not found in URL.</div>;
+    return (
+      <div className="flex items-center justify-center">
+        <p>Error: Note ID not found in URL.</p>
+      </div>
+    );
   }
 
   return (
     <section>
       <div className="max-w-5xl mx-auto px-6 md:px-4 py-8">
+        <div className="mb-4">
+          <button onClick={() => navigate(-1)} className="btn-primary">
+            <ChevronLeft className="icon-size" />
+          </button>
+        </div>
         <ViewAndEditNote NoteId={noteId} />
       </div>
     </section>
